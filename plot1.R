ff382fa02a0dafd41f8cdaa0e6bac05b54589dd5
Eashaan Godbole <egodbole@gmail.com>
2020-04-28 19:26:24 +0530

    Added timeStamp column separately

--- plot1.R
+++ plot1.R
@@ -11,10 +11,10 @@ library(lubridate)
 #data <- read.table("household_power_consumption.txt", sep=";")
 
 # converting dates and times to R formats
-#data$V1 <- dmy(data$V1)
+data$timeStamp <- with(data, dmy_hms(paste(V1, V2)))
 
 # selecting the required data for 2007-02-01 and 2007-02-01
-reqData <- data[data$V1==ymd("2007-02-01") | data$V1==ymd("2007-02-02"), ]
+reqData <- data[as.character(data$V1) == "1/2/2007" | data$V1 == "2/2/2007", ]
 reqData$V3 <- as.numeric(as.character(reqData$V3))
 
 # plotting
